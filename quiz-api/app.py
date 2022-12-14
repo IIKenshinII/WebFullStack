60c60328255b521f480fe22d563f6af0f640d76d
lKenshinl <62207548+lKenshinl@users.noreply.github.com>
2022-12-14 14:01:15 +0100

    postman test

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -4,6 +4,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/quiz-info', methods=['GET'])
+def GetQuizInfo():
+	return {"size": 0, "scores": []}, 200
+
 @app.route('/')
 def hello_world():
 	x = 'world'
