3d4dc5ef171fec85f7f8cd03b0d84b96ded50885
lKenshinl <62207548+lKenshinl@users.noreply.github.com>
2022-12-15 17:27:50 +0100

    Post login and token

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask,request
 from flask_cors import CORS
+from jwt_utils import *
 
 app = Flask(__name__)
 CORS(app)
@@ -11,7 +12,17 @@ def GetQuizInfo():
 @app.route('/')
 def hello_world():
 	x = 'world'
-	return f"Hello, {x}"
+	return f"Hey, {x}"
+
+@app.route('/login', methods=['POST'])
+def GetPassword():
+	payload = request.get_json()
+	password="flask2023"
+	token=build_token()
+	if password!=payload['password']:
+		return 'Unauthorized', 401
+	else :
+		return {"token":token},200
 
 if __name__ == "__main__":
     app.run()
